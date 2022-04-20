24ced597941defcafb4845cb7f430addd1748343
lisiyuan <10211100312@qq.com>
2022-04-20 23:22:49 +0800

    三个模块

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -18,26 +15,26 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult<Integer> create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
-        log.info("插入结果："+result);
+        log.info("插入结0果："+result+"0.0");
 
         if(result>0){
-            return new CommonResult(200,"insert success",result);
+            return new CommonResult<>(200,"insert success",result);
         }else{
-            return new CommonResult(444,"insert failed",null);
+            return new CommonResult<>(444,"insert failed",null);
         }
     }
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         Payment payment = paymentService.getPaymentById(id);
-        log.info("插入结果："+payment);
+        log.info("插入结果 热部署一下.："+payment);
 
         if(payment!=null){
-            return new CommonResult(200,"query success",payment);
+            return new CommonResult<>(200,"query success",payment);
         }else{
-            return new CommonResult(444,id+"query failed",null);
+            return new CommonResult<>(444,id+"query failed",null);
         }
     }
 }
